73ef524141ec14c87999423093ac9dea474146b0
Jay <jaykapadia389@gmail.com>
2023-05-03 23:39:55 +0530

    completed route of translator

--- components/translator.js
+++ components/translator.js
@@ -5,43 +5,69 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
-//   americanToBritish{
-//     k
-//   }
+  americanToBritish(text) {
 
-//   britishToAmerian{
-    
-//   }
+    let AToBDict = { ...americanOnly, ...americanToBritishSpelling };
+    let AToBTitles = americanToBritishTitles;
 
-  translat(text, dict ,titles){
-    let textLowerCase = text.toLowerCase;
-    let translate ;
-    const timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(:|\.)([0-5][0-9]))/g;
-
-     Object.entries(titles) 
-            .map(([key, value]) => {
-                if (textLowerCase.includes(key)) {
-                    translated = text.replace(new RegExp(key, "gi"), `<span class="highlight">${this.capitalizeFirstLetter(value)}</span>`) || text;
-                }
-            })
-
-        translated = translated || text;
-
-        if (changeTime) {
-            changeTime.map(time => {
-                translated = translated.replace(time, `<span class="highlight">${time.replace(':', '.')}</span>`) || text;
-            })
-        }
+    return this.translat(text, AToBDict, AToBTitles);
   }
 
-//   objectFlip{
-    
-//   }
+  britishToAmerian(text) {
 
-//   capitalizeFirstLetter{
-    
-//   }
-  
+    let BritishToAmericanSpelling = objectFlip(americanToBritishSpelling)
+    BToADict = { ...britishOnly, ...BritishToAmericanSpelling };
+    BToATitles = ObjectFlip(americanToBritishTitles);
+
+    return this.translat(text, BToADict, BToATitles);
+
+  }
+
+  translat(text, dict, titles) {
+    let textLowerCase = text.toLowerCase();
+    let translated;
+    const timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(:|\.)([0-5][0-9]))/g;
+
+    Object.entries(titles)
+      .map(([key, value]) => {
+        if (textLowerCase.includes(key)) {
+          translated = text.replace(new RegExp(key, "gi"), `<span class="highlight">${this.capitalizeFirstLetter(value)}</span>`) || text;
+        }
+      })
+
+    translated = translated || text;
+
+    const changeTime = textLowerCase.match(timeRegex);
+    if (changeTime) {
+      changeTime.map(time => {
+        translated = translated.replace(time, `<span class="highlight">${time.replace(':', '.')}</span>`) || text;
+      })
+    }
+
+    Object.entries(dict).map(([key, value]) => {
+
+      if (new RegExp('${key}', 'gi').test(textLowerCase)) {
+        translated = translated.replace(key, '<span class="highlight">${value}</span>') || text;
+      };
+
+      return translated || text;
+    })
+  }
+
+  objectFlip(obj) {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+      acc[value] = key;
+    }, {});
+  }
+
+  capitalizeFirstLetter(text) {
+    let arr = text.split('');
+
+    arr[0].toUpperCase();
+
+    return arr.join();
+  }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,45 @@ const Translator = require('../components/translator.js');
 module.exports = function (app) {
   
   const translator = new Translator();
+  console.log(translator);
+  console.log(translator.americanToBritish);
+  
 
   app.route('/api/translate')
     .post((req, res) => {
 
-      let {
-        text , locale 
-      } = req.body ; 
+      let { text , locale } = req.body ; 
 
-      let translation ;
+      if(!locale || text == undefined){
+        res.json({ error: 'Required field(s) missing' });
+        return;
+        
+      }
+      if(text == ""){
+        res.json({"error":"No text to translate"});
+        return;
+      }
 
+      if(locale != "american-to-british" && locale != "british-to-american"){
+        res.json({ error: 'Invalid value for locale field' });
+        return;
+      }
+
+      let translation;
+
+      if(locale == "american-to-british"){
+        translation = translator.americanToBritish(text);
+      }
+      else{
+        translation = translator.britishToAmerian(text); 
+      }
+
+
+      if(translation === text){
+        res.send("Everything looks good to me!");
+        return ;
+      }
+    
       res.json({
         text , translation
       });
